a3dc484457f134eedccfe42247208ca6fce4a16c
verdan kumar <vardantharwani@gmail.com>
2024-07-31 22:02:37 +0500

    Revising the concepts a bit

--- Programming Basic/Operator, Conditionals and Loops/operator.cpp
+++ Programming Basic/Operator, Conditionals and Loops/operator.cpp
@@ -4,7 +4,15 @@ using namespace std;
 
 
 int main(){
-
+    int age;
+        // if(cin >> age){
+        //     cout << "Hello" << endl;
+        // }
+        // if(cout << age);{
+        //     cout << "Hello" << endl;
+        // }
+    cin >> age ;
+    age > 20 ? cout << "You can vote" : cout << "You cant vote";
 
 
     return 0;
