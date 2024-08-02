bac98515502079e528ecfa7aa549421e21761725
verdan kumar <vardantharwani@gmail.com>
2024-08-02 22:58:10 +0500

    adding diamond pattern code

---
+++ Patterns/diamondPattern.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+
+void invertedParamid(int n){
+    for(int i=0; i<n; i++){
+        for(int j=0; j<i; j++){
+            cout << " ";
+        }
+        for(int j=0; j<n-i; j++){
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
+
+void fullParamid(int n){
+    for(int row=0; row<n; row++){
+        for(int col=0; col<n-row-1; col++){
+            cout << " ";
+        }
+        for(int col=0; col<=row; col++){
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
+
+
+int main(){
+    int n;
+    cin >> n;
+    fullParamid(n);
+    invertedParamid(n);
+
+
+    return 0;
+}
