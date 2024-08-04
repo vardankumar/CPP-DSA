5179e0c5c7f34e22fa845b70bbf68d80cb77b70a
verdan kumar <vardantharwani@gmail.com>
2024-08-05 00:05:56 +0500

    updating numeric Inverted Hollow Half Paramid code

--- Patterns/numericInvertedHollowHalfParamid.cpp
+++ Patterns/numericInvertedHollowHalfParamid.cpp
@@ -4,20 +4,22 @@ using namespace std;
 
 
 // 1 2 3 4 5
-// 2 3 4 5
-// 3 4 5 
+// 2     5
+// 3   5 
 // 4 5 
 // 5
 
 
 void numericInvertedHollowHalfParamid(int n){
     for(int i=0; i<n; i++){
-        int num = 1;
-        for(int j=1; j<=n-i; j++){
-            cout << i+j;
+        for(int j=i+1; j<=n; j++){
+            if(j == i+1 || j == n || i == 0){
+                cout << j << " ";
+            }else{
+                cout << "  ";
+            }
         }
         cout << endl;
-        // num = num + i;
     }
 
 }
